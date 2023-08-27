5db098cef607f065856bf74fd267dc7910768f32
Sander Verhagen <sander@sanderverhagen.net>
2023-08-27 00:39:48 -0700

    support-matchers: Support Hamcrest Matchers and other assertion strategies

--- test-shared/src/main/java/testlog/AbstractMutedLogAsserterExtensionIntegrationTest.java
+++ test-shared/src/main/java/testlog/AbstractMutedLogAsserterExtensionIntegrationTest.java
@@ -6,15 +6,35 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+
 public abstract class AbstractMutedLogAsserterExtensionIntegrationTest {
-    private static final Logger logger = LoggerFactory.getLogger(AbstractMutedLogAsserterExtensionIntegrationTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(
+            AbstractMutedLogAsserterExtensionIntegrationTest.class);
 
     @RegisterExtension
     public MutedLogAsserterExtension subject = new MutedLogAsserterExtension();
 
     @Test
-    public void test() {
+    public void testExpectByCount() {
+        try (ExpectedLogs ignored = subject.expect(2)) {
+            logger.error("expected log");
+            logger.warn("expected log");
+        }
+    }
+
+    @Test
+    public void testExpectByLevel() {
         try (ExpectedLogs ignored = subject.expect(Level.WARN)) {
+            logger.warn("expected log");
+        }
+    }
+
+    @Test
+    public void testExpectByMatcher() {
+        try (ExpectedLogs ignored = subject.expect(hasProperty("message", is(equalTo("expected log"))))) {
             logger.warn("expected log");
         }
     }
